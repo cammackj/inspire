7aa9dce25265a8515879139e9aaa212dd39bdd64
Joshua Cammack <cammack110@gmail.com>
2017-08-11 10:47:51 -0600

    first commit

--- public/app/components/quote/quote-controller.js
+++ public/app/components/quote/quote-controller.js
@@ -1,6 +1,7 @@
-function QuoteService(){
+function QuoteController(){
 
-	var qs = new QuoteService()
+	// TODO: CHANGE ALL VARIABLES TO QC
+	var qs = new QuoteController()
 
 	qs.getQuote(function(quote){
 		console.log('What is the quote', quote)
--- routes/todos.js
+++ routes/todos.js
@@ -1,1 +1,72 @@
 // You should put your todo schema should go here
+var express = require('express')
+var router = express.Router()
+var mongoose = require('mongoose')
+
+var todoSchema = new mongoose.Schema({
+	task: { type: String, required: true },
+	counter: { type: Number, default: 0}
+})
+
+var Todos = mongoose.model('Todo', todoSchema)
+
+router.get('/', function(req, res, next) {
+    Todos.find({})
+    .then((todos)=>{
+      res.send(todos)
+    })
+    .catch(next)
+})
+
+router.post('/', function(req, res, next) {
+    Todos.create(req.body)
+    .then((todo)=>{
+        res.send(todo)
+    })
+    .catch(next)
+})
+
+router.get('/:todoId', function(req, res, next){
+  let todoId = req.params.todoId
+  Todos.findById(todoId)
+  .then(todo =>{
+    if(todo){
+    res.send(todo)
+    }else{
+      next({message: 'You suck!'})
+    }
+  })
+  .catch(next)
+})
+
+router.delete('/:todoId', (req, res, next) => {
+  var todoId = req.params.todoId
+  Todos.findByIdAndRemove(todoId)
+  .then(todo =>{
+    res.send({message: 'Successfully deleted.'})
+  })
+  .catch(next)
+})
+
+router.put('/:todoId', (req, res, next) =>{
+  var todoId = req.params.todoId
+  var updatedTodoObj = req.body
+  Todos.findByIdAndUpdate(todoId, updatedTodoObj)
+  .then(todo => {
+    res.send({message: 'Successfully Updated Todo List'})
+  })
+  .catch(next)
+})
+
+
+router.use(defaultErrorHandler)
+
+function defaultErrorHandler(err, req, res, next){
+  if (req.xhr){
+    res.json({success: false, error: err})
+  }else{
+    res.json({success: false, error: err.message})
+  }
+}
+
+module.exports = router
