4e1a943f6f09d348cebcd0a50956e0cb3f01187d
Joshua Cammack <cammack110@gmail.com>
2017-08-15 15:59:28 -0600

    css update

--- public/app/components/weather/weather-controller.js
+++ public/app/components/weather/weather-controller.js
@@ -8,15 +8,10 @@ function WeatherController() {
 		var template = ''
 		var weatherElem = document.getElementById('weather')
 		template += `
-			<div class="container">
-				<div class="row">
-					<div class="col-md-offset-10">
 						<h3>Today's Weather</h3>
-						<p>${weather.name}</p>
-						<p>${weather.main.temp}</p>
-					</div>
-				</div>
-			</div>`
+						<p>${weather.name} Idaho</p>
+						<p>${weather.main.temp}&deg;F</p>
+				`
 		weatherElem.innerHTML = template
 	})
 
